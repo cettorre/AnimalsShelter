527a26791ee646f6be09ff0f296f222d37c21755
Unknown <c.ettorre@gmail.com>
2018-07-05 18:55:05 +0200

    working on view
    added code to main Activity for basic functionality
    modified InsertAnimalActivity layout

--- app/src/main/java/com/example/cettorre/animalsshelter/view/MainActivity.java
+++ app/src/main/java/com/example/cettorre/animalsshelter/view/MainActivity.java
@@ -1,15 +1,48 @@
 package com.example.cettorre.animalsshelter.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
 
 import com.example.cettorre.animalsshelter.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button mAddAnimal;
+    ListView mList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initComponents();
+
+
+        mAddAnimal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, InsertAnimalActivity.class);
+                startActivity(i);
+            }
+        });
+
+        mList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent i = new Intent(MainActivity.this, AnimalInfoActivity.class);
+                i.putExtra("position", position);
+                startActivity(i);
+            }
+        });
+    }
+
+    public void initComponents(){
+        mAddAnimal= (Button) findViewById(R.id.addAnimal);
+        mList = (ListView) findViewById(R.id.list);
     }
 }
