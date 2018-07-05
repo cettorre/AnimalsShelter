071572c1d933136a20e78ebaa4a03c6094418499
Unknown <c.ettorre@gmail.com>
2018-07-05 18:43:37 +0200

    activities view
    added activities into view
    modified main activity layout

---
+++ app/src/main/java/com/example/cettorre/animalsshelter/view/ShowPhotoActivity.java
@@ -0,0 +1,15 @@
+package com.example.cettorre.animalsshelter.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.cettorre.animalsshelter.R;
+
+public class ShowPhotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_photo);
+    }
+}
